e8852785834b38dfb542c9cf6858d56cef01853e
Aaron <aaronbee1990@gmail.com>
2024-12-16 18:42:53 -0300

    added quick tapping term

--- crkbd/keymaps/myMiryoku/keymap.c
+++ crkbd/keymaps/myMiryoku/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------------------------------------------.--------|            |-------------------------------------------------------------------------------------------------.
      KC_GRV,      KC_Q,        LGUI_T(KC_W), KC_E,        KC_R,        KC_T,       KC_LBRC,                   KC_RBRC,   KC_Y,        KC_U,         KC_I,        KC_O,           KC_P,            KC_NO,
   //|------------+------------+------------+------------+------------+------------|------------|            |-----------|------------+------------+------------+------------+-------------------+------------|
-     KC_EXLM,    KC_A,         ALT_T(KC_S),  CTL_T(KC_D), SFT_T(KC_F), KC_G,       KC_LPRN,                   KC_RPRN,   KC_H,        LSFT_T(KC_J), CTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_QUOT), KC_MINS,
+     KC_EXLM,    KC_A,         ALT_T(KC_S),  CTL_T(KC_D), SFT_T(KC_F), KC_G,       KC_LPRN,                   KC_RPRN,   KC_H,        LSFT_T(KC_J), CTL_T(KC_K), LALT_T(KC_L),   KC_QUOT,         KC_MINS,
   //|------------+------------+------------+------------+------------+------------|------------|            |-----------|------------+------------+------------+------------+-------------------+------------|
     KC_CIRC,      KC_Z,        RALT_T(KC_X), KC_C,        KC_V,        KC_B,                                             KC_N,        KC_M,         KC_COMM,     RALT_T(KC_DOT), KC_SLSH,  RALT(KC_COMM),
   //|------------+------------+------------+------------+------------+------------|                                     |------------+------------+------------+------------+-------------------+------------|
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
      XXXXXXX, KC_COLN, KC_DLR, KC_PERC,  KC_CIRC, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                               XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
+     XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                               XXXXXXX, KC_LSFT, KC_LCTL, KC_RALT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LPRN, KC_RPRN, KC_UNDS,            _______, XXXXXXX, XXXXXXX
                                       //`--------------------------'            `--------------------------'
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------------------------.           ,-------------------------------------------------------------.
      XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL, XXXXXXX,             XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
      XXXXXXX,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
