e2f0df8ff7a2f6cc1faaaaf1635a713a648d2f30
A2ron <aaronbee1990@gmail.com>
2025-10-29 14:55:24 -0300

    changed modifier key on number and symbol layer: shift, ctrl, alt; changed number layer: multiply and minus posicion; added modifiers on function layer

--- crkbd/keymaps/myMiryoku/keymap.c
+++ crkbd/keymaps/myMiryoku/keymap.c
@@ -87,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------------------------.           ,-------------------------------------------------------------.
      XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_COLN, KC_DLR, KC_PERC,  KC_CIRC, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX,
+     XXXXXXX, KC_COLN, KC_DLR, KC_PERC,  KC_CIRC, KC_PLUS, XXXXXXX,             XXXXXXX, XXXXXXX, LSFT_T(KC_LEFT), CTL_T(KC_DOWN), LALT_T(KC_UP),  KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                               XXXXXXX, KC_LSFT, KC_LCTL, KC_RALT, XXXXXXX, XXXXXXX,
+     XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                               XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LPRN, KC_RPRN, KC_UNDS,            _______, XXXXXXX, XXXXXXX
                                       //`--------------------------'            `--------------------------'
@@ -97,23 +97,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [5] = LAYOUT_split_3x6_3_ex2(
   //,-------------------------------------------------------------.           ,-------------------------------------------------------------.
-     XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     KC_ASTR, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,  XXXXXXX,             XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, XXXXXXX,
+     KC_ASTR, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,  XXXXXXX,             XXXXXXX, XXXXXXX, LSFT_T(KC_LEFT), CTL_T(KC_DOWN), LALT_T(KC_UP),  KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_GRV,     KC_1,    KC_2,     KC_3, KC_BSLS,                               KC_MINS, LSFT_T(KC_PPLS), CTL_T(KC_COMM), LALT_T(KC_DOT),   RGUI_T(KC_SLSH), XXXXXXX,
+     XXXXXXX, KC_GRV,     KC_1,    KC_2,     KC_3, KC_BSLS,                               KC_ASTR, KC_PPLS, KC_COMM, RALT_T(KC_DOT), KC_SLSH, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_0, KC_SPC, KC_TAB,                  XXXXXXX, _______, XXXXXXX
+                                          KC_0, KC_MINS, KC_TAB,                  XXXXXXX, _______, XXXXXXX
                                       //`--------------------------'            `--------------------------'
   ),
 
     [6] = LAYOUT_split_3x6_3_ex2(
   //,-------------------------------------------------------------.           ,-------------------------------------------------------------.
-     XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
+     XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI, XXXXXXX, QK_BOOT,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
      XXXXXXX,  KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL, XXXXXXX,             XXXXXXX, XXXXXXX, LSFT_T(KC_LEFT), CTL_T(KC_DOWN), LALT_T(KC_UP),  KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
-     XXXXXXX,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+     XXXXXXX,  KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,                               XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|           |--------+--------+--------+--------+--------+--------+--------|
                                           KC_APP, KC_SPC,   KC_TAB,             XXXXXXX, XXXXXXX, _______
                                       //`--------------------------'            `--------------------------'
